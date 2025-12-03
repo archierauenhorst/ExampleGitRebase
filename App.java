fe3ca75658ee79fffd9f7513333abfe9461969b5
archierauenhorst <archierauenhorst@u.boisestate.edu>
2025-12-03 16:10:32 -0700

    Revision 4 in Master

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     public void methodInMaster(){
         System.out.println("Revision 1 in Master");
         System.out.println("Revision 2 in Master");
+        System.out.println("Revision 4 in Master");
     }
 }
